b4fbf7c6faf1971a1e68c33bc6f0e60b0ba9846d
cauvang32 <vuquocanh1979@gmail.com>
2023-11-18 09:12:18 +0000

    q

--- 18112023/LYRICS/LYRICS.cpp
+++ 18112023/LYRICS/LYRICS.cpp
@@ -7,35 +7,43 @@ int n, m;
 int nm;
 string k;
 int minn = 1e9 + 1;
-int sosanh(string l, string k)
-{
-    int dem = 0;
-    for (int i = 0; i < min(l.size(), k.size()); i++)
-    {
-        if (l[l.size() - i] == k[k.size() - i])
-            dem++;
-        else
+int sosanh(const std::string& str1, const std::string& str2) {
+    int count = 0;
+    int len1 = str1.length();
+    int len2 = str2.length();
+    int minLength = (len1 < len2) ? len1 : len2;
+    
+    for (int i = 1; i <= minLength; ++i) {
+        if (str1[len1 - i] == str2[len2 - i] && str1[len1 - i]!='\n' && str2[len2 - i]!='\n') {
+            ++count;
+        } else {
             break;
+        }
     }
-    return dem;
+    if(count==0)return 0;
+    return count-1;
+}
+string clearss(string s)
+{
+    while(s[0]==' ')s.erase(s.begin());
+    while(s[n-1]==' ')s.erase(s[n-1]);
+    return s;
 }
 int main()
 {
     freopen("LYRICS.inp", "r", stdin);
     freopen("LYRICS.out", "w", stdout);
     cin >> n >> m;
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i <= n; i++)
     {
         cin.ignore();
-        getline(cin, s[i], '\n');
-        cin.ignore();
+        getline(cin, s[i]);        
     }
     for (int i = 1; i <= n; i++)
     {
-        for (int j = i + m; j <= n; j += m)
+        for (int j = i; j <= n; j += m)
         {
-            if (j <= n)
-                nm = sosanh(s[i], s[j]);
+            nm = sosanh(s[i], s[j]);
             if (nm > 0)
                 minn = min(sosanh(s[i], s[j]), minn);
         }
@@ -45,6 +53,9 @@ int main()
         cout << 0;
         return 0;
     }
-    cout << minn;
+    cout<<minn;
     return 0;
+    
+    
+   
 }
