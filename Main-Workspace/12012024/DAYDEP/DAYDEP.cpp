d3f0fad3fa809e47e719ae5baeccf502df145f36
cauvang32 <vuquocanh1979@gmail.com>
2024-01-13 16:05:00 +0700

    depzai

--- Main-Workspace/12012024/DAYDEP/DAYDEP.cpp
+++ Main-Workspace/12012024/DAYDEP/DAYDEP.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 #include <chrono>
 using namespace std;
-int xoa=0;
 int a[100001]; 
+int ts[10000001];
+vector <int> p;
 int n;
-int tmp=1,tmp1;
 int main()
 {
-    auto start = std::chrono::high_resolution_clock::now();
     freopen("DAYDEP.inp","r",stdin);
     freopen("DAYDEP.out","w",stdout);
     ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
@@ -15,29 +14,24 @@ int main()
     for(int i=1;i<=n;i++)
     {
         cin>>a[i];
+        if(ts[a[i]]==0)p.push_back(a[i]);
+        ts[a[i]]++;
     }
-    sort(a+1,a+n+1);
-    a[n+1]=1000000001;
-    tmp1=a[1];
-    for(int i=2;i<=n+1;i++)
+    int dem=0;
+    for(int i=0;i<p.size();i++)
     {
-        if(a[i]==a[i-1])
+        if(p[i]!=ts[p[i]])
         {
-            tmp++;
-        }
-        else
-        {
-            if(tmp!=tmp1)
+            if(p[i]>ts[p[i]])
             {
-                xoa+=tmp;
+                dem+=ts[p[i]];
             }
-            tmp1=a[i];
-            tmp=1;
+            else if(p[i]<ts[p[i]])
+            {
+                dem+=ts[p[i]]-p[i];
+            }
         }
     }
-    cout<<xoa<<' ';
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    std::cout << "Time taken by function: " << duration.count() / 1000000.0 << " seconds" << std::endl;
+    cout<<dem;
     return 0;
 }
---
+++ Main-Workspace/13012024/MUAVE/MUAVE.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int tong1=0;
+int tong2=0;
+int minn=1e9+1;
+int a[1000001];
+int main()
+{
+    freopen("MUAVE.inp","r",stdin);
+    freopen("MUAVE.out","w",stdout);
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    cin>>n;
+    for(int i=1;i<=n;i++)cin>>a[i];
+    for(int i=n;i>2;i--)tong2+=a[i];
+    tong1=a[1];
+    for(int i=2;i<n;i++)
+    {
+        minn=min(abs(tong1-tong2),minn);
+        tong1+=a[i];
+        tong2-=a[i+1];
+    }
+    cout<<minn;
+}
